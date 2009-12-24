12499140aa92ce4032d7cb76c45dec9a11acbea8
Darren Torpey <dct@punchbowlsoftware.com>
2009-12-24 11:59:56 -0500

    Updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
   def page_title
     @page_title || 'AuntieDeborah.com'
   end
+
+  def mail_to_mom
+    email_address = 'aquacranial@gmail.com'
+    # mail_to 'aquacranial@gmail.com', nil, :replace_at => "_at_", :replace_dot => "_dot_", :class => "email", :style => 'text-decoration: none; color: #006FB9;'
+    mail_to email_address, email_address, :style => 'text-decoration: none; color: #006FB9;'
+  end
 end
